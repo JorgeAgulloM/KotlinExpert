970e429f141df4e636792950021c1d17b27892c0
Jorge1 <agullojorge@gmail.com>
2024-09-17 08:45:13 +0200

    Data default

--- composeApp/src/desktopMain/kotlin/Note.kt
+++ composeApp/src/desktopMain/kotlin/Note.kt
@@ -1,6 +1,10 @@
 import Note.Type.*
 
-data class Note(val title: String, val description: String, val type: Type = TEXT) {
+data class Note(
+    val title: String,
+    val description: String = "default description",
+    val type: Type = TEXT
+) {
     enum class Type { TEXT, AUDIO }
 }
 
@@ -10,4 +14,11 @@ fun getNotes(): List<Note> = (1..10).map {
         description = "Description $it",
         type = if (it % 3 == 0) AUDIO else TEXT
     )
+}
+
+fun test() {
+    Note("Title")
+    Note("Title", "Description")
+    Note("Title", "Description", AUDIO)
+    Note(title = "Title", type = AUDIO)
 }
