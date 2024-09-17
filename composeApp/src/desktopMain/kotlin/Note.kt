f43171baf01501b63d17c0a1439c9d251c1684da
Jorge1 <agullojorge@gmail.com>
2024-09-17 08:39:51 +0200

    Data classes

---
+++ composeApp/src/desktopMain/kotlin/Note.kt
@@ -0,0 +1,13 @@
+import Note.Type.*
+
+data class Note(val title: String, val description: String, val type: Type) {
+    enum class Type { TEXT, AUDIO }
+}
+
+fun getNotes(): List<Note> = (1..10).map {
+    Note(
+        title = "title $it",
+        description = "Description $it",
+        type = if (it % 3 == 0) AUDIO else TEXT
+    )
+}
