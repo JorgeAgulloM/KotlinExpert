0258e8cd3a807318945ae540c890dac3401da3cf
Jorge1 <agullojorge@gmail.com>
2024-09-23 20:03:40 +0200

    Import and Type Alias

--- composeApp/src/desktopMain/kotlin/extras/KotlinImportAlias.kt
+++
@@ -1,6 +0,0 @@
-package extras
-
-import data.Note as DataNote
-import extras.Note as ExtrasNote
-
-fun ExtrasNote.toData(): DataNote = DataNote(title, description)
---
+++ composeApp/src/desktopMain/kotlin/extras/KotlinImportAndTypeAlias.kt
@@ -0,0 +1,11 @@
+package extras
+
+import data.Note as DataNote
+import extras.Note as ExtrasNote
+
+typealias DataNoteTyped = data.Note //Estos son públicos para toda la app, pero no sustituye al original, se pueden usar indistintamente
+private typealias ExtraNoteTyped = extras.Note // Tambien se puede hacer privada
+
+fun ExtrasNote.toData(): DataNote = DataNote(title, description)
+
+fun ExtraNoteTyped.toDataTyped(): DataNoteTyped = DataNoteTyped(title, description)
